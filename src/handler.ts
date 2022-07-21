7cd0d0ee6db3261df656944dcffa1c2be6c44136
ghdmsrkd <ghdmsrkd@naver.com>
2022-07-22 01:23:40 +0900

    feat : connect, disconnect 로직 추가

--- src/handler.ts
+++ src/handler.ts
@@ -1,14 +1,30 @@
-exports.websocketHandler = (event: any, context: any, callback: any) => {
-    console.log(event.requestContext.eventType)
-    console.log(event.requestContext.connectionId)
-    return {
-        isBase64Encoded: true,
-        statusCode: 200,
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            "Access-Control-Expose-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-        },
-        body: "ok"
+import * as ddb from "./common/ddb"
+import UserRepository from "./common/ddb/user/user.repo"
+
+exports.websocketHandler = async (event: any, context: any, callback: any) => {
+    console.log(`${event.requestContext.eventType} : ${event.requestContext.connectionId}`)
+    try {
+        const userRepo = UserRepository.getInstance()
+        if(event.requestContext.eventType === "CONNECT"){
+            const user = await userRepo.createUserById(event.requestContext.connectionId)
+            console.log(user)
+        } else {
+            const user = await userRepo.deleteUserById(event.requestContext.connectionId)
+            console.log(user)
+        }
+        
+        return {
+            isBase64Encoded: true,
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Access-Control-Expose-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: "ok"
+        }
+    } catch (error) {
+        console.log(error)
     }
+    
 }
