0dafea8f82f7222b1be614c5cb8e8255124e1c46
ghdmsrkd <ghdmsrkd@naver.com>
2022-07-22 03:14:47 +0900

    feat : send message action 추가

--- src/handler.ts
+++ src/handler.ts
@@ -1,18 +1,27 @@
 import * as ddb from "./common/ddb"
 import UserRepository from "./common/ddb/user/user.repo"
+import * as AWS from "aws-sdk"
+
+const apiGatewayManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: 'http://localhost:3001',
+});
 
 exports.websocketHandler = async (event: any, context: any, callback: any) => {
+    // console.log(event)
     console.log(`${event.requestContext.eventType} : ${event.requestContext.connectionId}`)
+
     try {
         const userRepo = UserRepository.getInstance()
         if(event.requestContext.eventType === "CONNECT"){
             const user = await userRepo.createUserById(event.requestContext.connectionId)
             console.log(user)
+            
         } else {
             const user = await userRepo.deleteUserById(event.requestContext.connectionId)
             console.log(user)
         }
-        
+
         return {
             isBase64Encoded: true,
             statusCode: 200,
@@ -25,6 +34,28 @@ exports.websocketHandler = async (event: any, context: any, callback: any) => {
         }
     } catch (error) {
         console.log(error)
+    }
+}
+
+exports.sendMessageHandler = async (event: any, context: any, callback: any) => {
+    // console.log(event)
+    console.log(`${event.requestContext.eventType} : ${event.requestContext.connectionId}`)
+
+    const dt = { ConnectionId: event.requestContext.connectionId, Data: JSON.stringify("메시지~") };
+    try {
+        await apiGatewayManagementApi.postToConnection(dt).promise();
+        return {
+            isBase64Encoded: true,
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Access-Control-Expose-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: "ok"
+        }
+    } catch (error) {
+        console.log(error);
     }
     
 }
